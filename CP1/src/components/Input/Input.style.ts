b567f393d4f13e0e8ad0316aece68fbb424c1bfd
PedroSouza1111 <pedrohenriqueds0511@gmail.com>
2024-09-08 03:15:38 -0300

    projeto final com formulário finalizado

--- CP1/src/components/Input/Input.style.ts
+++ CP1/src/components/Input/Input.style.ts
@@ -15,3 +15,22 @@ export const StyledDiv = styled.div`
     margin-left: 1.2rem;
     margin-right: 1rem;
 `
+
+export const StyledSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin-top: 0.6rem;
+`
+
+export const StyledArticle = styled.article`
+    display: flex;
+    border-bottom: 1px solid #D0D0D0;
+    margin: 0.5rem 1.2rem;
+    padding-bottom: 0.7rem;
+    padding-left: 0.4rem;
+    align-items: center;
+`
+
+export const StyledParagraph = styled.p`
+    margin-left: 0.5rem;
+`
--- CP1/src/pages/List/List.tsx
+++ CP1/src/pages/List/List.tsx
@@ -1,13 +1,32 @@
 import { BiPlusCircle } from "react-icons/bi";
 import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
-import { StyledDiv } from "../../components/Input/Input.style";
+import { StyledArticle, StyledDiv, StyledParagraph, StyledSection } from "../../components/Input/Input.style";
 import { Label } from "../../components/Label/Label";
 import { Layout } from "../../components/Layout/Layout";
 import { ListTitle } from "../../components/Title/ListTitle";
 import { FormStyledDiv } from "../../components/Button/Button.style";
+import { useState } from "react";
+import { FaRegCircle } from "react-icons/fa";
 
 export default function List () {
+
+    const [content, setContent] = useState<string>("");
+    const [hasContent, setHasContent] = useState<boolean>(false)
+    const [values, setValues] = useState<string[]>([])
+
+    const handleContent = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setContent(event.target.value)
+    }
+
+    const handleClick = () => {
+        if (content != "") {
+            setHasContent(true)
+            setValues([...values, content])
+            setContent("")
+    }
+}
+
     return (
         <Layout>
             <ListTitle />
@@ -19,16 +38,29 @@ export default function List () {
                             type="text"
                             id="list"
                             name="list"
-                            onChange={console.log}
+                            onChange={handleContent}
+                            value={content}
                             placeholder="Digite o item desejado"
                             />
                     </StyledDiv>
-                    <Button type="button" onClick={console.log}>
+                    <Button type="button" onClick={handleClick}>
                         <BiPlusCircle size="2.2rem" color="#3893de"/>
                     </Button>
                 </FormStyledDiv>
             </form>
-
+                {hasContent 
+                ? (
+                    <StyledSection>
+                    {values.map((value, index) => (
+                    <StyledArticle key={index}>
+                        <FaRegCircle size="1.2rem" color="#3893de" />
+                        <StyledParagraph>{value}</StyledParagraph>
+                    </StyledArticle>
+                            ))}
+                    </StyledSection>
+                ) 
+                : (null)
+                }
         </Layout>
     )
 }
